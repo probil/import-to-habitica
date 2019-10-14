215b5a920e1ce488b2ee0db68b6fa82c6f15e349
Max Lyashuk <m_lyashuk@ukr.net>
2019-10-14 17:54:54 +0300

    feat(popup): configure `postcss`, `autoprefixer` and `tailwindcss`

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,17 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project
+  content: ['./popup/index.html', './popup/*.vue'],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
 module.exports = {
-  plugins: {
-    'tailwindcss': 'tailwind.config.js',
-    '@fullhuman/postcss-purgecss': process.env.NODE_ENV === 'production',
-    'autoprefixer': {}
-  }
+  plugins: [
+    require('tailwindcss'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : [],
+    require('autoprefixer')
+  ]
 };
